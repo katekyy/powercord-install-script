13890062a6cfa5e7dce99df2919a26a5322b3a85
katekyy <74244551+katekyy@users.noreply.github.com>
2022-09-03 19:47:44 +0200

    FIX: I'm bad at this

--- win/uninstall.ps1
+++ win/uninstall.ps1
@@ -45,7 +45,7 @@ try {
 }
 
 Write-Host "Starting Discord Canary..." -ForegroundColor blue
-Invoke-Expression "C:\Users\$env:USERNAME\AppData\Local\DiscordCanary\app-*\DiscordCanary.exe" | Out-Null
+Invoke-Expression "'C:\Users\$env:USERNAME\AppData\Local\DiscordCanary\app-*\DiscordCanary.exe'" | Out-Null
 
 Start-Sleep -s 2
 Write-Host "Done! [8x00000]" -ForegroundColor green
